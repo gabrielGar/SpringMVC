fee8ffb1ccaec1b8f932b7558e4ad2d847a87dd7
Gabriel <gabriel.garcia.gar@gmail.com>
2021-09-23 15:59:07 +0200

    Ej 1-6: Fibonacci in HTML table

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -37,6 +37,31 @@ public class HelloController {
 
     @GetMapping("/fibonacci/{numElements}")
     public ArrayList<Integer> GetFibonacciList(@PathVariable("numElements") int numElements){
+        return GetFibonacciNumbers(numElements);
+    }
+
+    
+    @GetMapping("/fibonacci/{numElements}/table")
+    public String GetFibonacciTable(@PathVariable("numElements") int numElements){
+        ArrayList<Integer> fiboNumbers = GetFibonacciNumbers(numElements);
+        StringBuilder b = new StringBuilder();
+        b.append("<table style='width:100%'>");
+        b.append("<tr>");
+        for (int i = 0; i < fiboNumbers.size(); i++) {
+            b.append("<td>");
+            b.append(fiboNumbers.get(i));
+            b.append("</td>");
+            if ((i+1) % 4 == 0){
+                b.append("</tr>");
+                b.append("<tr>");
+            } 
+        }
+        b.append("</tr>");
+        b.append("</table>");
+        return b.toString();
+    }
+
+    private ArrayList<Integer> GetFibonacciNumbers(int numElements){
         ArrayList<Integer> result = new ArrayList<Integer>();
         int firstVal = 1, lastVal = 1;
         if (numElements > 0){
