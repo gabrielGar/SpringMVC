80559bd560c71e0095e5e3082aa810cb289752ef
Gabriel <gabriel.garcia.gar@gmail.com>
2021-09-23 15:50:26 +0200

    Ej 1-5: Secuencia de Fibonacci de N elementos

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +34,23 @@ public class HelloController {
     public Integer[] IntegerList(){
         return new Integer[] {1,2,3,4,5};
     }
+
+    @GetMapping("/fibonacci/{numElements}")
+    public ArrayList<Integer> GetFibonacciList(@PathVariable("numElements") int numElements){
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int firstVal = 1, lastVal = 1;
+        if (numElements > 0){
+            int curElement = 0, helpVal = 0;
+            while(numElements > curElement){
+                result.add(firstVal);
+                curElement++;
+                helpVal = lastVal;
+                lastVal += firstVal;
+                firstVal = helpVal; 
+            }
+        }
+        return result;
+    }
+
     
 }
