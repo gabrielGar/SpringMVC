4aa3f5dc488a012c00d5c4beed3f50d663ab30f1
Gabriel <gabriel.garcia.gar@gmail.com>
2021-09-26 11:30:09 +0200

    Ej 2-7: GET /v1/posts

--- src/main/java/com/example/demo/PostsController.java
+++ src/main/java/com/example/demo/PostsController.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PostsController {
     private static ArrayList<Post> posts = new ArrayList<Post>(Arrays.asList(
         new Post(1, 1, "Mensaje de prueba 1"),
@@ -10,4 +15,27 @@ public class PostsController {
         new Post(3, 1, "Mensaje de prueba 3"),
         new Post(4, 2, "Mensaje de prueba 4")
     ));
+
+    @GetMapping("/v1/posts")
+    public ArrayList<Post> GetAll(@RequestParam(
+                        name="userId",
+                        required=false, 
+                        defaultValue="0") int userId){
+        ArrayList<Post> result = getPostsByUserId(userId);
+        
+        return result;
+    }
+
+    private ArrayList<Post> getPostsByUserId(int userId){
+        ArrayList<Post> result = posts;
+        if(userId != 0){
+            result = new ArrayList<Post>();
+            for (Post post : posts) {
+                if (post.getUserId() == userId)
+                    result.add(post);
+            }
+        }
+        return result;
+    }
+
 }
