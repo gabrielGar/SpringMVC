c67c7e88d151eb934fde6d86a1c2873aafa3326c
Gabriel <gabriel.garcia.gar@gmail.com>
2021-09-26 11:09:07 +0200

    Ej 2-1: GET /v1/users

--- src/main/java/com/example/demo/UsersController.java
+++ src/main/java/com/example/demo/UsersController.java
@@ -3,7 +3,12 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsersController {
     public static List<UserV2> users = new ArrayList<UserV2>(Arrays.asList(
         new UserV2(1, "Usuario 1", "en-US"),
@@ -13,4 +18,13 @@ public class UsersController {
         new UserV2(5, "Usuario 5", "en-US"),
         new UserV2(6, "Usuario 6", "es-ES")
     ));
+
+    
+    @GetMapping("/v1/users")
+    public List<User> Users(){
+        return users
+                    .stream()
+                    .map(u -> new User(u.getId(), u.getName()))
+                    .collect(Collectors.toList());
+    }
 }
